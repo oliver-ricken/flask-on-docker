e10634a7faf2a2e67e46920a78830853ce759572
Oliver Ricken <2oliver.ricken@gmail.com>
2024-02-12 12:33:17 -0800

    completed up through dev test section, beta works!

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask, jsonify, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+import os
 
+from flask import (
+    Flask,
+    jsonify,
+    send_from_directory,
+    request,
+)
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
@@ -26,3 +33,23 @@ def hello_world():
 @app.route("/static/<path:filename>")
 def staticfiles(filename):
     return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+
+@app.route("/media/<path:filename>")
+def mediafiles(filename):
+    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+    return """
+    <!doctype html>
+    <title>upload new File</title>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    """
